f521c211dcda99b657ac503c6c2a578fa537576a
Naim Adaimy <nadaimy@globalistic.tech>
2023-06-04 17:52:31 +0300

    minor changes4

--- src/app/sampleData.ts
+++ src/app/sampleData.ts
@@ -5,19 +5,19 @@ let activities =
   displayTextMsg:'Progress',
   displayTextValue:'Status',
    ListOfPlans: [
-    {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'100%',value:{value:'Completed', status:'completed'}},
-    {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'xxx',value:{value:'Ongoing', status:'on'}},
-    {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'xxx',value:{value:'Not Started', status:'off'}},
-    {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'xxx',value:{value:'Ongoing', status:'on'}},
+    {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'100%',value:{value:'Completed', status:'completed'}},
+    {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'xxx',value:{value:'Ongoing', status:'on'}},
+    {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'xxx',value:{value:'Not Started', status:'off'}},
+    {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'xxx',value:{value:'Ongoing', status:'on'}},
   ]};
 
   let indicators =
   {title: 'Indicators', subTitle: 'Indicator', displayTextMsg:'Base-line', displayTextValue:'Target',
   ListOfPlans: [
-   {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'50',value:{value:'5000', status:null}},
-   {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'xxx',value:{value:'xxx', status:null}},
-   {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'xxx',value:{value:'xxx', status:null}},
-   {plan:'Lorem Ipsum is simply dummy text of the printing and typesetting industry',message:'xxx',value:{value:'xxx', status: null}},
+   {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'50',value:{value:'5000', status:null}},
+   {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'xxx',value:{value:'xxx', status:null}},
+   {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'xxx',value:{value:'xxx', status:null}},
+   {plan:'Lorem Ipsum is simply dummy text of the printing ',message:'xxx',value:{value:'xxx', status: null}},
  ]};
 
  let comments = [
@@ -29,7 +29,7 @@ let activities =
   {
     name:'Alice',
     imgUrl:'../../../../../assets/profilePic.png',
-    comment:'Lorem Ipsum is simply dummy text of the printing and typesetting industry'
+    comment:'Lorem Ipsum is simply dummy text of the printing '
   },
  ];
 
